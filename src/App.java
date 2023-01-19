4b21d8b9a7ce7650fb03be637f289505ee44e4ab
Luan Lima <luan_lima2011@hotmail.com>
2023-01-18 22:31:19 -0300

    Adicionado construtores e metodos Taxplay

--- src/App.java
+++ src/App.java
@@ -1,42 +1,61 @@
+import entities.TaxPlayer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
-import entities.TaxPlayer;
-
 public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner sc  = new Scanner(System.in);
 
-        TaxPlayer taxPlayer = new TaxPlayer();
-        List<TaxPlayer> list = new ArrayList<>();
+  public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    Locale.setDefault(Locale.US);
+    List<TaxPlayer> list = new ArrayList<>();
 
-        System.out.print("Quantos contribuintes voce vai digitar? :");
-        int num = sc.nextInt();
-        
-        for (int i = 0; i < num; i++) {
-            System.out.println("Digite os dados do "+(i+1)+"ª contribuinte: ");
+    System.out.print("Quantos contribuintes voce vai digitar? ");
+    int num = sc.nextInt();
 
-            System.out.print("Renda anual com salário: ");
-            double salary = sc.nextDouble();
+    for (int i = 0; i < num; i++) {
+      System.out.println("Digite os dados do " + (i + 1) + "ª contribuinte: ");
 
-            System.out.print("Renda anual com prestação de servico: ");
-            double services = sc.nextDouble();
+      System.out.print("Renda anual com salário: ");
+      double salary = sc.nextDouble();
 
-            System.out.print("Renda anual com ganho de capital: ");
-            double capital = sc.nextDouble();
+      System.out.print("Renda anual com prestação de servico: ");
+      double services = sc.nextDouble();
 
-            System.out.print("gastos médicos: ");
-            double health = sc.nextDouble();
+      System.out.print("Renda anual com ganho de capital: ");
+      double capital = sc.nextDouble();
 
-            System.out.print("gastos educacionais: ");
-            double education = sc.nextDouble();
+      System.out.print("gastos médicos: ");
+      double health = sc.nextDouble();
 
-            TaxPlayer tax = new TaxPlayer(salary, services, capital, health, education);
+      System.out.print("gastos educacionais: ");
+      double education = sc.nextDouble();
 
-            list.add(tax);
-        }
+      TaxPlayer tax = new TaxPlayer(
+          salary,
+          services,
+          capital,
+          health,
+          education);
 
-        sc.close();
+      list.add(tax);
+     
+    clearConsole();
     }
+
+    int i = 0;
+    for (TaxPlayer x : list) {
+      System.out.println("Resumo do " + (i + 1) + "º contribuinte:");
+      System.out.println(x);
+      i++;
+    }
+
+    sc.close();
+  }
+  public static void clearConsole(){
+    // limpa o terminal java.
+      System.out.print("\033[H\033[2J");                    
+      System.out.flush(); 
+  }
 }
--- src/entities/TaxPlayer.java
+++ src/entities/TaxPlayer.java
@@ -1,22 +1,95 @@
 package entities;
 
+
+
 public class TaxPlayer {
-    private Double salaryIncome;
-    private Double servicesIncome;
-    private Double capitalIncome;
-    private Double healthSpeding;
-    private Double educationSpeding;
 
-    public TaxPlayer(){}
+  private Double salaryIncome;
+  private Double servicesIncome;
+  private Double capitalIncome;
+  private Double healthSpeding;
+  private Double educationSpeding;
 
-    public TaxPlayer(Double salaryIncome, Double servicesIncome, Double capitalIncome, Double healthSpeding,
-            Double educationSpeding) {
-        this.salaryIncome = salaryIncome;
-        this.servicesIncome = servicesIncome;
-        this.capitalIncome = capitalIncome;
-        this.healthSpeding = healthSpeding;
-        this.educationSpeding = educationSpeding;
+  public TaxPlayer() {
+  }
+
+  public TaxPlayer(
+      Double salaryIncome,
+      Double servicesIncome,
+      Double capitalIncome,
+      Double healthSpeding,
+      Double educationSpeding) {
+    this.salaryIncome = salaryIncome;
+    this.servicesIncome = servicesIncome;
+    this.capitalIncome = capitalIncome;
+    this.healthSpeding = healthSpeding;
+    this.educationSpeding = educationSpeding;
+  }
+
+  public Double getSalaryIncome() {
+    return salaryIncome;
+  }
+
+  public Double getServicesIncome() {
+    return servicesIncome;
+  }
+
+  public Double getCapitalIncome() {
+    return capitalIncome;
+  }
+
+  public Double getHealthSpeding() {
+    return healthSpeding;
+  }
+
+  public Double getEducationSpeding() {
+    return educationSpeding;
+  }
+
+  public Double salaryTax() {
+    if (this.salaryIncome / 12 < 3000) {
+      return 0.0;
+    }else if(this.salaryIncome / 12 <= 5000){
+      return getSalaryIncome() * 10 / 100;
+    }else{
+      return getSalaryIncome() * 20 / 100;
     }
-
-    
+  }
+  public Double servicesTax(){
+    if(getServicesIncome() == 0.00){
+      return 0.0;
+    }else{
+      return getServicesIncome() * 15 / 100;
+    }
+  }
+  public Double capitalTax(){
+    if(getCapitalIncome() == 0){
+      return 0.0;
+    }else{
+      return getCapitalIncome() * 20 / 100;
+    }
+  }
+  public Double grossTax(){
+    return salaryTax() + servicesTax() + capitalTax();
+  }
+  public Double taxRebate(){
+    if((grossTax() * 30 / 100) < educationSpeding + healthSpeding){
+      return grossTax() * 30 / 100;
+    }else{
+      return educationSpeding + healthSpeding;
+    }
+  }
+  public Double netTaX(){
+    return grossTax() - taxRebate();
+  }
+  public String toString() {
+    return "Imposto bruto total: "
+    + String.format("%.2f%n",grossTax())
+    + "Abatimento : "
+    + String.format("%.2f%n",taxRebate())
+    + "Imposto devido: "
+    + String.format("%.2f%n",netTaX())
+    + '\n';
+  }
+  
 }
